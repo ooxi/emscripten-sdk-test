a0e0537cb762bb8e846d67b47edb23018d2d39f9
ooxi <violetland@mail.ru>
2016-04-13 20:31:00 +0200

    test

--- lib/travis/build.sh
+++ lib/travis/build.sh
@@ -4,8 +4,10 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 
-echo "Will use \`$PWD' as source directory"
+SOURCE_DIRECTORY=$PWD
+TRAVIS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Will use \`${SOURCE_DIRECTORY}' as source and \`${TRAVIS_DIRECTORY}\' as travis directory"
 
 docker pull thewtex/cross-compiler-browser-asmjs
-docker run --rm -it -v $PWD:/usr/src:rw thewtex/cross-compiler-browser-asmjs /usr/bin/find .
+docker run --rm -it -v $SOURCE_DIRECTORY:/usr/src:rw -v $TRAVIS_DIRECTORY:/travis:ro thewtex/cross-compiler-browser-asmjs /travis/test.sh
 
---
+++ lib/travis/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo "Inside docker!"
+find .
+
