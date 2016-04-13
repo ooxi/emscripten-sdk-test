015f0b0b92253ff2e3f70fc6c09f38142c000695
ooxi <violetland@mail.ru>
2016-04-13 18:34:13 +0200

    Debug...

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -y  build-essential cmake python2.7 nodejs default-jre
 
 wget 'https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz'
-tar -xvf emsdk-portable.tar.bz
+tar -xvf emsdk-portable.tar.gz
 
 cd emsdk-portable
 ./emsdk update
