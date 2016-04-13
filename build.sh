19c0c0902c2a89f826838f90d4bf187c11a2d906
ooxi <violetland@mail.ru>
2016-04-13 20:10:25 +0200

    dev

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 
+mkdir build
+
 docker pull thewtex/cross-compiler-browser-asmjs
-docker run --rm -it thewtex/cross-compiler-browser-asmjs
+docker run --rm -it -v .:ro -v ./build:rw thewtex/cross-compiler-browser-asmjs /bin/ls -la
 
