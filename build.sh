3b6eb8566dd23e2072c32d85729e096907278ebb
ooxi <violetland@mail.ru>
2016-04-13 19:16:09 +0200

    testing...

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y emscripten
 mkdir build
 cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=/usr/share/emscripten/cmake/Platform/Emscripten.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=/usr/share/emscripten/cmake/Platform/Emscripten.cmake ..
 make
 ls -la
 
