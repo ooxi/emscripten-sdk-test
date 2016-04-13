3cc51713ce9f8a2ccb9b9fdcbd2e480093d8e08d
ooxi <violetland@mail.ru>
2016-04-13 20:53:37 +0200

    docu

--- lib/travis/build.sh
+++ lib/travis/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 
+# Install docker
 sudo apt-get update
 sudo apt-get install -y docker.io
 
+# Specify environment
 SOURCE_DIRECTORY=$PWD
 TRAVIS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Will use \`${SOURCE_DIRECTORY}' as source and \`${TRAVIS_DIRECTORY}\' as travis directory"
 
+# Compile to JavaScript
 docker pull thewtex/cross-compiler-browser-asmjs
 docker run --rm -it -v $SOURCE_DIRECTORY:/usr/src:rw -v $TRAVIS_DIRECTORY:/travis:ro thewtex/cross-compiler-browser-asmjs /travis/compile-inside-docker.sh
 
-find .
+# Test
+sudo apt-get install -y nodejs
 nodejs build/hello-world.js
 
