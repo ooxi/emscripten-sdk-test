8cc78669eb93bacd8301070191fcd6689df4a427
ooxi <violetland@mail.ru>
2016-04-13 19:07:51 +0200

    Using native package?

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 
 
 sudo apt-get update
-sudo apt-get install -y  build-essential cmake python2.7 nodejs default-jre
+sudo apt-get install -y emscripten
 
-wget 'https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz'
-tar -xvf emsdk-portable.tar.gz
-
-cd emsdk_portable
-./emsdk update
-./emsdk install latest
+cmake -DCMAKE_TOOLCHAIN_FILE=/usr/share/emscripten/cmake/Platform/Emscripten.cmake .
+make
+ls
 
