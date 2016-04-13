e6d2de9573e3f985bd9b83418f54b02f59ba8789
ooxi <violetland@mail.ru>
2016-04-13 20:21:01 +0200

    test

--- lib/travis/build.sh
+++ lib/travis/build.sh
@@ -5,7 +5,10 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 
 mkdir build
+echo $PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
 
-docker pull thewtex/cross-compiler-browser-asmjs
-docker run --rm -it -v .:/usr/src/input:ro -v ./build:/usr/src/output:rw thewtex/cross-compiler-browser-asmjs /usr/bin/find .
+#docker pull thewtex/cross-compiler-browser-asmjs
+#docker run --rm -it -v .:/usr/src/input:ro -v ./build:/usr/src/output:rw thewtex/cross-compiler-browser-asmjs /usr/bin/find .
 
